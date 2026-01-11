bee2230d0167145d1de911c40b4d5a9ffb98086d
LukeSeobyungju <22100356@handong.ac.kr>
2026-01-11 23:53:28 +0900

    예제 코드

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -7,7 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ReviewController {
 
+    /*
+    TODO: 리뷰 컨트롤러 완성하기
 
+    () 안에 있는 내용은 입력 받아야하는 정보
+    1. 리뷰 생성 (메뉴 번호, 리뷰 점수, 리뷰 내용)
+    2. 리뷰 리스트 조회 (메뉴 번호)
+    3. 리뷰 수정 (리뷰 번호, 리뷰 점수, 리뷰 내용)
+    4. 리뷰 삭제 (리뷰 번호)
+
+    적절히 필요한 DTO 생성
+
+     */
 
 
 }
--- src/main/java/com/example/demo/service/ReviewService.java
+++ src/main/java/com/example/demo/service/ReviewService.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReviewService {
 
+    /*
+    TODO: 리뷰 서비스 완성하기
 
+    컨트롤러와 동일하게 기능 완성
+    어려울 땐 메뉴 서비스 참고
+
+     */
 
 }
